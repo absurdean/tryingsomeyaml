30b43e234f4394af86dcba6c99246dbd2bc1df22
4poKer <arseniy59@yandex.ru>
2017-02-25 14:43:01 +0300

    Add SimpleUserControlFromYaml branch
    Add Simple UserControl which get data from yaml config file

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApplication1.Helpers;
+using WpfApplication1.Models;
 using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
@@ -29,98 +31,12 @@ namespace WpfApplication1
     public partial class MainWindow : Window
     {
 
-        public class TextBox
-        {
-            public string Id { get; set; }
-            public string Caption { get; set; }
-            public string Size { get; set; }
-        }
-        public class CheckBox
-        {
-            public string Id { get; set; }
-            public string Label { get; set; }
-            public string Size { get; set; }
-        }
-        public class RadioButtonGroup
-        {
-            public string Id { get; set; }
-            public string Label { get; set; }
-            public string Orientation { get; set; }
-            public List<string> OptionList { get; set; }
-        }
-
-
+       
         public MainWindow()
         {
             InitializeComponent();
 
-            StreamReader sr = new StreamReader("example1.yaml");
-            string text = sr.ReadToEnd();
-            var input = new StringReader(text);
-
-            // Load the stream
-            var yaml = new YamlStream();
-            yaml.Load(input);
-
-            // Examine the stream
-            var mapping =
-                (YamlMappingNode) yaml.Documents[0].RootNode;
-
-            var items = (YamlSequenceNode) mapping.Children[new YamlScalarNode("form-elements")];
-
-            foreach (YamlMappingNode item in items)
-            {
-
-                var type = item.Children[new YamlScalarNode("type")];
-
-                switch (type.ToString())
-                {
-                    case "textbox":
-                    {
-
-                        var textboxItem = new TextBox()
-                        {
-                            Id = item.Children[new YamlScalarNode("id")].ToString(),
-                            Caption = item.Children[new YamlScalarNode("caption")].ToString(),
-                            Size = item.Children[new YamlScalarNode("size")].ToString(),
-                        };
-
-                        break;
-
-                    }
-
-                    case "checkbox":
-                    {
-                        var checkboxItem = new CheckBox()
-                        {
-                            Id = item.Children[new YamlScalarNode("id")].ToString(),
-                            Label = item.Children[new YamlScalarNode("label")].ToString(),
-                            Size = item.Children[new YamlScalarNode("size")].ToString(),
-                        };
-                        break;
-                    }
-                    case "radiobuttons":
-                    {
-                        var optionListItems = (YamlSequenceNode) item.Children[new YamlScalarNode("optionList")];
-                        var list = new List<string>();
-                        foreach (YamlScalarNode x in optionListItems)
-                        {
-                            list.Add(x.ToString());
-                        }
-                        var radiobuttonsItem = new RadioButtonGroup()
-                        {
-                            Id = item.Children[new YamlScalarNode("id")].ToString(),
-                            Label = item.Children[new YamlScalarNode("label")].ToString(),
-                            Orientation = item.Children[new YamlScalarNode("orientation")].ToString(),
-                            OptionList = list,
-                        }
-                        ;
-                        break;
-                    }
-                }
-
-
-            }
+            DataContext = new ViewModels.MainWindowViewModel();
 
         }
     }
---
+++ WpfApplication1/WpfApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplication1.Helpers;
+using WpfApplication1.Models;
+
+namespace WpfApplication1.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion INotifyPropertyChanged
+
+        //пример textBoxControl'a в который биндятся данные из yaml
+        public TextBoxControlViewModel TextBoxEx { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+            //список моделей с данными, получаемых из yaml
+            var controls = YamlDriver.GetObjects(@"..\..\Resources\YamlConfig.yaml");
+
+            //пример инициализации textBoxControl'a
+            TextBoxEx = new TextBoxControlViewModel(controls[0] as TextBoxModel);
+
+           
+        }
+    }
+}
